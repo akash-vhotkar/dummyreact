aa8fafef7c3bd21d1ccaa06651d6c791128c05ba
akash127 <akash@iotric.com>
2021-09-29 21:34:33 +0530

    done

--- src/config/Env.js
+++ src/config/Env.js
@@ -1,6 +1,6 @@
 export const ENVIROMENT = "DEV";
 
-export const API_URL = ENVIROMENT === "DEV" ? "https://holypenniesapi.herokuapp.com" : "https://blog.hardcipher.io";
+export const API_URL = ENVIROMENT === "DEV" ? "http://localhost:9000" : "https://blog.hardcipher.io";
 export const APP_URL = ENVIROMENT === "DEV" ? "https://holypennies.herokuapp.com/" : "https://admin.hardcipher.io";
 
 
--- src/config/axios.js
+++ src/config/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 import { API_URL } from './Env';
 import store from '../store';
 import { LOG_OUT, SET_TOKEN } from '../store/constant';
-
 const prevTOken = localStorage.getItem('jwt');
-
 const axiosinstance = axios.create({
     baseURL: API_URL,
 });
